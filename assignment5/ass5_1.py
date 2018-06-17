b12a04e978cc831df0f30662af4f5262473459a3
malay190 <malaykumar089@gmail.com>
2018-06-17 23:09:06 +0530

    done

--- assignment5/ass5_1.py
+++ assignment5/ass5_1.py
@@ -8,19 +8,12 @@ if year%4==0:
 			print("entered number is a leap year",1
 		else :
 			print("entered number is not a leap year",)
+	else :
+		print("entered number is not a leap year",)
 else :
 	print("entered number is not a leap year",)
 	
-#method-2
 
-year=int(input("enter any year:"))
-if year%4==0:
-	if year%100==0 and year%400==0:
-		print("entered number is a leap year")
-	else:
-		print("entered number is not a leap year")
-else:
-	print("entered number is not a leap year")
 
 
 		
--- assignment6/ass6_2.py
+++ assignment6/ass6_2.py
@@ -1,4 +1,4 @@
 #Q.2- Write an infinite loop.An infinite loop never ends. Condition is always true. 
 i=1
-while i<2:
+while i<=10:
 	 print("Hello World!")
